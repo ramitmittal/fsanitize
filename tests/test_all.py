99a7d49957ea3f06b1ec94c6565a3c7a8f10885d
Ramit Mittal <ramit.m@yahoo.com>
2018-05-03 22:48:31 +0530

    migrated to pytest
    added pytest to travis.yml

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,48 +1,27 @@
-import unittest
+import pytest
 import os
 import shutil
 from filename_sanitizer import sanitize
 
 
-class Test00(unittest.TestCase):
-    """Tests the name_maker function."""
+@pytest.mark.parametrize("str00, str01", [('A&*(Kkdf)  ', 'a___kkdf___'),
+                        ('A&*DF8876-=__', 'a__df8876____'),
+                        ('   -   .ex', '_______.ex'),
+                        ('Lord of the Rings - The Return of The King.mp4', 'lord_of_the_rings___the_return_of_the_king.mp4')])
+def test_name_maker(str00, str01):
+    assert sanitize.name_maker(str00) == str01
 
-    def test_name_maker(self):
+@pytest.fixture
+def dir_set_up():
+    os.mkdir('test_dir')
+    os.mkdir('test_dir/SOES44')
+    open('test_dir/taydsd&&{a.mp3', 'a').close()
+    open('test_dir/ii**IIIp3', 'a').close()
+    open('test_dir/SOES44/8U^ut.txt', 'a').close()
+    yield
+    shutil.rmtree('test_dir')
 
-        str00 = 'A&*(Kkdf)  '
-        str01 = 'A&*DF8876-=__'
-        str02 = '   -   .ex'
-        str03 = 'Lord of the Rings - The Return of The King.mp4'
-
-        str10 = 'a___kkdf___'
-        str11 = 'a__df8876____'
-        str12 = '_______.ex'
-        str13 = 'lord_of_the_rings___the_return_of_the_king.mp4'
-
-        self.assertEqual(sanitize.name_maker(str00), str10)
-        self.assertEqual(sanitize.name_maker(str01), str11)
-        self.assertEqual(sanitize.name_maker(str02), str12)
-        self.assertEqual(sanitize.name_maker(str03), str13)
-
-
-class Test01(unittest.TestCase):
-    """Tests the recursive_rename function, which is basically everything."""
-
-    def setUp(self):
-        os.mkdir('test_dir')
-        os.mkdir('test_dir/SOES44')
-        open('test_dir/taydsd&&{a.mp3', 'a').close()
-        open('test_dir/ii**IIIp3', 'a').close()
-        open('test_dir/SOES44/8U^ut.txt', 'a').close()
-
-    def tearDown(self):
-        shutil.rmtree('test_dir')
-
-    def test_everything(self):
-        sanitize.recursive_rename('test_dir')
-        self.assertEqual(os.listdir('test_dir'), ['soes44', 'ii__iiip3', 'taydsd___a.mp3'])
-        self.assertEqual(os.listdir('test_dir/soes44'), ['8u_ut.txt'])
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_everything(dir_set_up):
+    sanitize.recursive_rename('test_dir')
+    assert os.listdir('test_dir') == ['soes44', 'ii__iiip3', 'taydsd___a.mp3']
+    assert os.listdir('test_dir/soes44') == ['8u_ut.txt']
